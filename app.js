a5a128348c40ae23235dbab5e41d3246860cfdbe
danjool <horrabin@gmail.com>
2025-02-08 21:36:15 -0700

    serve noir images

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => { // expanded cert with  sudo certbot certonly --sta
 	if (host.startsWith('wat.')) {
 	  	express.static(path.join(__dirname, 'public/wat'))(req, res, next);
 	} else if (host.startsWith('noir.')){
+		app.use('/images', express.static(path.join(__dirname, 'public/images')));
 		res.sendFile(path.join(__dirname, 'public/noir.html'));
 	} else {
 	  next();
