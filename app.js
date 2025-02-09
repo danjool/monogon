79c927f256cf926b348cf5ab1054e4993b36948e
danjool <horrabin@gmail.com>
2025-02-08 17:01:44 -0700

    wat sub domain as host express lookup

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const socketio = require('socket.io')
 let pool, credentials
 
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => { // expanded cert with  sudo certbot certonly --standalone -d monogon.net -d wat.monogon.net, added to wat. cname to Route 53, value is monogon.net
+	const host = req.get('host');
+	if (host.startsWith('wat.')) {
+	  express.static(path.join(__dirname, 'public/wat'))(req, res, next);
+	} else {
+	  next();
+	}
+});
+
 app.use(express.json())
 
 try {
