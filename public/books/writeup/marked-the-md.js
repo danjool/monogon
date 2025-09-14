e9fb42b78af8d769305c8d5e7764186c33d963a9
danjool <dcmartin@gmail.com>
2025-09-14 18:44:36 +0200

    books writeup iteration, mobile friendly?

--- public/books/writeup/marked-the-md.js
+++ public/books/writeup/marked-the-md.js
@@ -2,26 +2,49 @@
   const fs = require('fs');
 
   const styles = `<style>
+body {
+  background-color: cadetblue;
+  margin: 4vw; 
+  max-width: 100%;
+  box-sizing: border-box;
+  font-size: 1vw;
+}
+@media (max-width: 1400px) {
   body {
-    background-color: cadetblue;
-    margin: 66px;
+    font-size: 4vw;
+    background-color: burlywood;
   }
-  img {
-    max-width: 80%;
-    display: block;
-    margin: auto;
-  }
-  h4 {
-    font-style: italic;
-    text-align: center;
-    color: #555555; 
-  }
-  pre {
-    background-color: bisque;
-  }
+}
+
+img {
+  max-width: 80%; /* Changed from 80% */
+  height: auto; /* Maintain aspect ratio */
+  display: block;
+  margin: auto;
+}
+
+h4 {
+  font-style: italic;
+  text-align: center;
+  color: #555555; 
+}
+
+pre {
+  background-color: bisque;
+  overflow-x: auto;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  padding: 10px; 
+  margin: auto;
+}
+
+p, li, td {
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+}
 </style>`;
 
   const markdown = fs.readFileSync('./books-writeup.md', 'utf8');
   const html = marked.parse(markdown);
   const styledHtml = styles + html;
-  fs.writeFileSync('./writeup.html', styledHtml);
+  fs.writeFileSync('./index.html', styledHtml);
